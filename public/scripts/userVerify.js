8a2fe1c639e63cd7fdb5abb5280fed4200c3580f
Saud <saudkhan03@outlook.com>
2025-04-13 19:16:12 +0530

    fix: update user verification to use POST method and correct URL structure

--- public/scripts/userVerify.js
+++ public/scripts/userVerify.js
@@ -1,22 +1,32 @@
 $(document).ready(() => {
     var urlParams = new URLSearchParams(window.location.search);
     var code = urlParams.get('code');
-    var basePath = 'https://masjidnearme.azurewebsites.net/v1/users/'
+    var basePath = 'https://masjidnear.me/v1/users/'
     var u = '';
     if (!code || code.length == 0) {
         showErrorState()
     }
     else {
-        u = `${basePath}verify/${code}`;
+        u = `${basePath}verify`;
     }
+    let obj = { "verificationCode": code };
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: u,
-        crossDomain: true,
-        cache: true,
-        dataType: 'JSON',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(obj),
         success: function (data, status) {
-            showSuccessState(data)
+            if (status != 'success') {
+                showErrorState()
+                return;
+            }
+            if (data && data.error) {
+                showErrorState()
+                return;
+            }
+            showSuccessState(data.user);
+            loginUser(data.user);
         },
         error: function (errordata) {
             showErrorState()
@@ -39,8 +49,22 @@ function showErrorState() {
 
     redirect();
 }
+function loginUser(user) {
+    let token = request.getResponseHeader('x-auth-token')
+    if (token) {
+        //store the token in cookie
+        $.cookie('token', token);
+        //store the token in session
+        sessionStorage.setItem('token', token);
+        sessionStorage.setItem('userEmail', data.user.userEmail)
+        sessionStorage.setItem('userSettings', btoa(JSON.stringify(data.user.settings)))
+        sessionStorage.setItem('userProfile', btoa(JSON.stringify(data.user.userprofile)))
+        checkUserLoggedIn(data.user);
+        clearMarkers(true);
+    }
+}
 function redirect() {
     setTimeout(() => {
-        // window.location.href = 'https://masjidnear.me'
+        window.location.href = 'https://masjidnear.me'
     }, 5000);
 }
