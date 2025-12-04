b2ff7163b4e8196a3cf911a070500d72f27bf756
Saud <saudkhan03@outlook.com>
2025-12-04 12:52:08 +0530

    increase search radius increment and limit for masjid search

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -390,9 +390,9 @@ function getMasjids(lt, ln, radius) {
                 $('#mnu').flmnu({
                     items: [],
                 });
-                if (radius < 10000) {
-                    showAlert(`No masjids found within ${radius / 1000} km. Expanding search radius to ${(radius / 1000) + 3} km`, 2500)
-                    getMasjids(lt, ln, radius + 3000)
+                if (radius < 20000) {
+                    showAlert(`No masjids found within ${radius / 1000} km. Expanding search radius to ${(radius / 1000) + 5} km`, 2500)
+                    getMasjids(lt, ln, radius + 5000)
                 }
                 else {
                     showAlert('No masjids found', 3000)
